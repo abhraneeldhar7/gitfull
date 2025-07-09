9197e5b9c95c23751575cf5523c63d2e7f3a6bcb
Abhraneel <abhraneeldhar7@gmail.com>
2025-07-10 01:26:47 +0530

    css tweak

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
       </div>
 
 
-      <div className="min-h-[500px]">
+      <div className="min-h-[500px] mt-[50px]">
 
         <h1 className={styles.sectionTitle}>
           Not your average gpt wrapper
