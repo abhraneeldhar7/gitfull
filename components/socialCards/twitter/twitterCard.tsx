737d887d113a054f29094a2878f314ffa2bdd659
Abhraneel <abhraneeldhar7@gmail.com>
2025-07-20 22:06:27 +0530

    lp fix

--- components/socialCards/twitter/twitterCard.tsx
+++ components/socialCards/twitter/twitterCard.tsx
@@ -5,7 +5,7 @@ export default function TwitterCard() {
     return (<div className={`${styles.cardMain} border border-[1px] border-[var(--foreground)]/30`}>
         <EllipsisIcon size={20} className="absolute right-[10px] top-[7px] opacity-[0.5]" />
         <div className="w-[40px]">
-            <Image src="/socialCards/MrRobot.png" width={40} height={40} alt="" className="min-w-[40px] object-cover h-[40px] rounded-[50%]" unoptimized />
+            <Image src="\socialCards\mrRobot.png" width={40} height={40} alt="" className="min-w-[40px] object-cover h-[40px] rounded-[50%]" unoptimized />
         </div>
 
         <div className="flex flex-col gap-[5px]">
