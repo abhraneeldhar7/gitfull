a986287181a0e059ac0431c04df7945bebcbf87e
Abhraneel <abhraneeldhar7@gmail.com>
2025-07-20 16:47:34 +0530

    eh

--- app/api/credit-tokens/route.ts
+++ app/api/credit-tokens/route.ts
@@ -1,7 +1,7 @@
 import { creditTokensToAll } from "@/app/actions/mongodbFunctions";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method not allowed" });
     }
